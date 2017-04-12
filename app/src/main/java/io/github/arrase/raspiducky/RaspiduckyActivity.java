1fc3c9d642b16d769ccef9d123d308a61933f407
arrase <arrase@gmail.com>
2017-04-12 22:03:31 +0200

    first commit

--- app/src/main/java/io/github/arrase/raspiducky/RaspiduckyActivity.java
+++ app/src/main/java/io/github/arrase/raspiducky/RaspiduckyActivity.java
@@ -31,8 +31,8 @@ public class RaspiduckyActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (getFragmentManager().getBackStackEntryCount() > 0)
-            getFragmentManager().popBackStack();
+        if (mFragmentManager.getBackStackEntryCount() > 0)
+            mFragmentManager.popBackStack();
         else
             super.onBackPressed();
     }
